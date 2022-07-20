f61f9a13f28013ee801771ebdd1fe19be9ece1c3
paityn-farrow <paityn.farrow@cdpipelinedevshop.com>
2022-07-20 16:20:15 +0000

    helpdesk form

---
+++ sign-in/src/components/SignIn.js
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { link } from "react-router-dom";
+import { purple } from '@mui/material/colors';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ bgcolor: purple[500] }} variant="circle">
+  <AssignmentIcon />
+</Avatar>
+          <Typography component="h1" variant="h5">
+            Help Desk
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            
+          <TextField 
+                id="First Name*" 
+                label="First Name*" 
+                variant="outlined" 
+              />
+
+          <TextField 
+                id="Last Name*" 
+                label="Last Name*" 
+                variant="outlined" 
+              />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="Description"
+              label="Description"
+              type="Description"
+              id="Description"
+              
+              />
+            
+          
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+               Submit
+            </Button>
+            <Grid container>
+            
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Experiencing issues? Call our Support Hotline"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
